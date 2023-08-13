dbedbb5ccdbac47baedaf30e25d719d0d5ac759a
chensyf <1278651995@qq.com>
2023-08-13 19:36:13 +0800

    delete

--- internal/storage/memory/delete.go
+++ internal/storage/memory/delete.go
@@ -1,7 +1,23 @@
 package memory
 
-import "github.com/chensylz/goredis/internal/protocol"
+import (
+	"github.com/chensylz/goredis/internal/global/serrors"
+	"github.com/chensylz/goredis/internal/protocol"
+)
 
 func (m *Memory) delete(args []*protocol.ProtoValue) *protocol.ProtoValue {
-	return nil
+	if len(args) < 2 {
+		return serrors.NewErrSyntaxIncorrect()
+	}
+	var count int
+	for _, arg := range args[1:] {
+		if _, ok := m.data[arg.Value.(string)]; ok {
+			count++
+		}
+		delete(m.data, arg.Value.(string))
+	}
+	return &protocol.ProtoValue{
+		Type:  protocol.Integer,
+		Value: count,
+	}
 }
