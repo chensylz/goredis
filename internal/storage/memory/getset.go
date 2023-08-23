f1f66a6fcb392e9c9c6abec62168010d2e57bab4
chensyf <1278651995@qq.com>
2023-08-23 20:25:59 +0800

    hit

--- internal/storage/memory/getset.go
+++ internal/storage/memory/getset.go
@@ -19,10 +19,14 @@ func (m *Memory) get(args []*protocol.ProtoValue) *protocol.ProtoValue {
 	if len(args) != 1 {
 		return serrors.NewErrSyntaxIncorrect()
 	}
-	m.RWMutex.RLock()
-	defer m.RWMutex.RUnlock()
-	if v, ok := m.data[args[0].Value.(string)]; ok {
-		return serrors.NewBulkString(v.Value.(string))
+	m.Lock()
+	defer m.Unlock()
+	key := args[0].Value.(string)
+	v, ok := m.data[key]
+	if !ok {
+		return serrors.NewNilBulk()
 	}
-	return serrors.NewNilBulk()
+	v.Hit++
+	m.data[key] = v
+	return serrors.NewBulkString(v.Value.(string))
 }
